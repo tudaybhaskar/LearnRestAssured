dc01829b84c76d914671985d906a669917e66364
tudaybhaskar <udayabhaskar110@gmail.com>
2024-01-17 19:13:19 +0530

    Updated DemoClass

--- src/main/java/com/app/DemoClass.java
+++ src/main/java/com/app/DemoClass.java
@@ -5,5 +5,6 @@ public class DemoClass {
     public static void main(String args) {
 
         System.out.println(" Testing application for github pollSCM For every 3 minutes");
+        System.out.println(" WebHook testing");
     }
 }
--- src/test/java/com/spotify/oauth2/tests/DemoTest.java
+++ src/test/java/com/spotify/oauth2/tests/DemoTest.java
@@ -12,7 +12,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class DemoTest {
 
-    private String access_token = "BQCzHoQEvxQjBJhWbHM1zwOwOD6FAgrJ8id8hMauo57rwgJet6TOXdbRvBCPY8qzTkUaAD50cqQYLZan_hBbdEDK08GOWU3snYG5OgcsPnlAercrfEE0AVW1XES1wvW6Ut8TmjzBC3Q170UCY315cvn5QjEKyQ-QcQo9HJBTHxmCFc2wLGNtqtTl8xBrcm-I9Yufz6kZYK4y12QlLmzrMVrPkncwGQBjmb11rAek8d8VfWO8UBzaHnyKKJceTrHNpcOfuTQaTMVN";
+    private String access_token = "BQCYGYUVzdVTlRcAoxZPOY1WukKhjhFLObgkUMc0VQRBfdWpQQUS62wpd0hO7b8887nqMDInjX_gNK55CCgcPAuQVuIzvTwI_SZaUmNznOaanwQzHqBhRHRP9nD5CaLQbHcssIurZqwZbaYOME7pAluoOLOLDAikbv71l2DtXnuwgnfDeLCQuveuY9QIs4egowSS79_k1aYLxSDmPPGlN5jaqLqVjzZGiw7c4iJkmGkKp9l9luYIi4Veb54hsnwFORxV3RVWrb7F";
 
     @Test
     public void getCurrentUserId(){
