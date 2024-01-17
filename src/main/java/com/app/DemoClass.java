e7f74120b24df0efc9aedc0c85d3749b8b03b57b
tudaybhaskar <udayabhaskar110@gmail.com>
2024-01-17 18:38:18 +0530

    Updated DemoClass

--- src/main/java/com/app/DemoClass.java
+++ src/main/java/com/app/DemoClass.java
@@ -2,8 +2,8 @@ package com.app;
 
 public class DemoClass {
 
-    public static void main(String args){
+    public static void main(String args) {
 
-        System.out.println(" Testing application for github pollSCM");
+        System.out.println(" Testing application for github pollSCM For every 3 minutes");
     }
 }
