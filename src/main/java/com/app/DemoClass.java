410957f5541cc011794b36590b72cc738b759297
tudaybhaskar <udayabhaskar110@gmail.com>
2024-01-17 18:27:07 +0530

    Updated DemoClass

--- src/main/java/com/app/DemoClass.java
+++ src/main/java/com/app/DemoClass.java
@@ -4,6 +4,6 @@ public class DemoClass {
 
     public static void main(String args){
 
-        System.out.println(" Testing application for Github Build trigger option");
+        System.out.println(" Testing application for github pollSCM");
     }
 }
