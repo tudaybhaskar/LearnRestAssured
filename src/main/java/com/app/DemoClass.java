f3505123f8d45fb5960f3b64ab2c9e2b7c3ebf2e
tudaybhaskar <udayabhaskar110@gmail.com>
2024-01-17 17:21:21 +0530

    Updated test app class

--- src/main/java/com/app/DemoClass.java
+++ src/main/java/com/app/DemoClass.java
@@ -3,6 +3,7 @@ package com.app;
 public class DemoClass {
 
     public static void main(String args){
-        System.out.println(" Testing application");
+
+        System.out.println(" Testing application for Github Build trigger option");
     }
 }
